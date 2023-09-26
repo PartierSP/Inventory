cdc35f1d71dd26dac40a135f1a394d91501844f4
PartierSP <mikevl1973@gmail.com>
2023-09-25 20:49:55 -0400

    Fixed typo

--- inc_variables.php
+++ inc_variables.php
@@ -1,6 +1,6 @@
 <?php
 
 	$debug=false;							//Enable or disable database debug mode 
-	$serveraddress='http://s.lan/i';		//Inventory system address for use with QR Codes.
+	$serveraddress='http://s.lan/i/';		//Inventory system address for use with QR Codes.
 
 ?>
