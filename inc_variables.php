21affcf71f1d7a3415178ed2a6bcf3840c9cacd7
PartierSP <mikevl1973@gmail.com>
2023-09-25 20:22:59 -0400

    Updated server address

--- inc_variables.php
+++ inc_variables.php
@@ -1,6 +1,6 @@
 <?php
 
 	$debug=false;							//Enable or disable database debug mode 
-	$serveraddress='http://i.lan/i';		//Inventory system address for use with QR Codes.
+	$serveraddress='http://s.lan/i';		//Inventory system address for use with QR Codes.
 
 ?>
